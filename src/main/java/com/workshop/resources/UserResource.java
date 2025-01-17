bdd841b26dfa91cc5959d6f85905891af37e3ebe
Guilherme Campos <guiicampos31@gmail.com>
2025-01-17 19:35:36 -0300

    Obtendo um usuário por id

--- src/main/java/com/workshop/resources/UserResource.java
+++ src/main/java/com/workshop/resources/UserResource.java
@@ -6,11 +6,11 @@ import com.workshop.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,5 +26,12 @@ public class UserResource {
         List<User> list = userService.findAll();
         List<UserDTO> users = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
--- src/main/java/com/workshop/services/UserService.java
+++ src/main/java/com/workshop/services/UserService.java
@@ -2,10 +2,12 @@ package com.workshop.services;
 
 import com.workshop.domain.User;
 import com.workshop.repository.UserRepository;
+import com.workshop.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
